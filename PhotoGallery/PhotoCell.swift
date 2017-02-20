4981f59d2ac5161eec8b55c294fcd0c39cc4b067
Ahmet Alsan <ahmet.alsan@nomadcommerce.com>
2017-02-20 14:26:36 +0300

    collection view added to show flickr images

---
+++ PhotoGallery/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoGallery
+//
+//  Created by Ahmet Alsan on 20/02/2017.
+//  Copyright © 2017 photogallery. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
--- PhotoGallery/ViewController.swift
+++ PhotoGallery/ViewController.swift
@@ -8,20 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
+    var photos:[Photo]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+
         let flickrProvider = FlickrProvider()
         flickrProvider.getPhotosByTag(tag: "moda") { photos in
-            
+            self.photos = photos
+            self.collectionView?.reloadData()
         }
+        
+        let screenWidth = UIScreen.main.bounds.width
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: screenWidth/3, height: screenWidth/3)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        collectionView!.collectionViewLayout = layout
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos?.count ?? 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath) as! PhotoCell
+        
+        cell.imageView.image = photos?[indexPath.row].image
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("You selected cell #\(indexPath.item)!")
     }
 
 
